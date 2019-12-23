934f5434fd0a20662818d05c9bc19b0310204bc9
Eliezer Castro <eliezergino@gmail.com>
2019-12-23 20:34:33 -0300

    ScrollView e RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -27,36 +27,51 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Peso (Kg)',
-              labelStyle: TextStyle(color: Colors.green),
+      body: SingleChildScrollView(
+        padding:EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Peso (Kg)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0,
+              ),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Altura (Cm)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0,
+              ),
             ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Altura (Cm)',
-              labelStyle: TextStyle(color: Colors.green),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50,
+                child: RaisedButton(
+                  onPressed: (){},
+                  child: Text('Calcular', style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                  color: Colors.green,
+                ),
+              ),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          ),
-        ],
+            Text('Info', textAlign: TextAlign.center, style: TextStyle(color: Colors.green, fontSize: 25.0),),
+          ],
+        ),
       ),
     );
-  }
+  } 
 }
